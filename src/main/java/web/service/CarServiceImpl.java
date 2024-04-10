aa46f54202f12310b1e514da742dd2b3bad31a44
grinderos <grinderos.coding@gmail.com>
2024-04-10 22:14:32 +0300

    2.2.2 done

--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -3,7 +3,6 @@ package web.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import web.dao.CarDAO;
-import web.dao.CarDAOImpl;
 import web.model.Car;
 
 import java.util.List;
